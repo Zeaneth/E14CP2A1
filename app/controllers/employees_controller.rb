98219e049285b43f1d74c3160a26d3377432a09c
Zamiz <zamiztobar@gmail.com>
2018-12-06 20:20:54 -0300

    +strong params

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,11 @@
 class EmployeesController < ApplicationController
+    def create
+
+    end
+
+    private
+
+      def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :email)
+      end
 end
