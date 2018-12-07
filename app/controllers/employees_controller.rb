0aa6d3dd4fe93e50e60efbd22142fcea21184f37
Zamiz <zamiztobar@gmail.com>
2018-12-06 21:42:22 -0300

    fix employee controller's create method add list of employees to show in company's view fix form view to add all fields

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,12 +1,16 @@
 class EmployeesController < ApplicationController
     def create
         @employee = Employee.new(employee_params)
-        redirect_to company_path
+        if @employee.save!
+            redirect_to company_employees_path(company)
+        else
+            render :new
+        end
     end
 
     private
 
       def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :email)
+        params.require(:employee).permit(:first_name, :last_name, :email, :company_id, :area_id)
       end
 end
