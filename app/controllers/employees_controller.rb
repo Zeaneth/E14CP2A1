215e19544077312ce5013e68e1c79f1d4d6da23e
Zamiz <zamiztobar@gmail.com>
2019-01-22 02:00:09 -0300

    +create and destroy methods to employee controler +add table to show employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,28 @@
 class EmployeesController < ApplicationController
+    before_action :find_company, only: [:create, :destroy]
     def create
         @employee = Employee.new(employee_params)
+        @employee.company = @company
         if @employee.save!
-            redirect_to company_employees_path(company)
+            redirect_to @company
         else
             render :new
         end
     end
 
-    private
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy
 
+        redirect_to company_path(@company)
+    end
+
+    private
       def employee_params
         params.require(:employee).permit(:first_name, :last_name, :email, :company_id, :area_id)
+      end
+
+      def find_company
+        @company = Company.find(params[:company_id])
       end
 end
